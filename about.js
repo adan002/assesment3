7a4d84857ff1c784d7d04bb70d1de21a690bff58
adan villalpando <adan_1990.icloud.com>
2022-06-10 13:02:25 -0600

    initial commit

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	window.alert('form has been submitted successfully');
+}
+	let test = document.getElementById("https://www.elinkdesign.com/assets/files/Page_Editor_Files/20180418_105737.jpg");
+	"https://www.elinkdesign.com/assets/files/Page_Editor_Files/20180418_105737.jpg".addEventListener("mouseenter", function( event ) 
+	
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
