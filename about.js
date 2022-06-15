708a03da828e665604557d0294439dd262682ded
adan villalpando <adan_1990.icloud.com>
2022-06-14 21:48:00 -0600

    second push

--- about.js
+++ about.js
@@ -1,16 +1,26 @@
 console.log("hello world");
 
+let ducky = document.querySelector("#ducky")
+
+const duckyMessage = () => {
+	alert('You did a mouseover')
+}
+
+ducky.addEventListener('mouseover', duckyMessage)
 
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
 	window.alert('form has been submitted successfully');
-}
-	let test = document.getElementById("https://www.elinkdesign.com/assets/files/Page_Editor_Files/20180418_105737.jpg");
-	"https://www.elinkdesign.com/assets/files/Page_Editor_Files/20180418_105737.jpg".addEventListener("mouseenter", function( event ) 
+	
 	
 }
+
+function createNewEvent(event) {
+
+}
+
 
 
 let form = document.querySelector('#contact');
---
+++ profile.js
@@ -0,0 +1,30 @@
+//fav color function
+
+let colorBtn = document.querySelector('#color')
+
+const favColor = () => {
+    alert('gold')
+}
+
+colorBtn.addEventListener('click', favColor)
+
+
+//fav place function
+
+let placeBtn = document.querySelector('#place')
+
+const favPlace = () => {
+    alert('The ocean')
+}
+
+placeBtn.addEventListener('click', favPlace)
+
+// fav ritual
+
+let ritualBtn = document.querySelector('#ritual')
+
+const favRitual = () => {
+    alert('meditating')
+}
+
+ritualBtn.addEventListener('click', favRitual)
